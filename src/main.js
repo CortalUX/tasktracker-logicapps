2818cff73ef5293294526468d8cd3f0d062f3638
Chris Lloyd-Jones <chris.lloyd-jones@avanade.com>
2018-03-01 23:03:11 -0500

    Added reply functionality

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 const url_actions="https://prod-04.uksouth.logic.azure.com:443/workflows/56254cab7b6c407f91aa7a94a9ef549d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d33x49ZsEIF7xa-u5Q9BKU2Adf6jlnCJRtwwDd2hvoM";
-// type, conversationid
+// const for sending actions
 const url_emails="https://prod-29.uksouth.logic.azure.com:443/workflows/b136a37912e6424590967522192b3b35/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=askQTpkRwhuZH70FPRiqCXgCyhrbMfiI6GxwtLOFuI4";
-// nada
+// const for getting emails
+const email_sig=`<div class="WordSection1"><p class="MsoNormal"><span style="font-size:11.0pt">&nbsp;</span></p><p class="MsoNormal"><span style="font-size:11.0pt">&nbsp;</span></p><p class="MsoNormal"><span lang="EN-US" style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:#FF5800">Chris Lloyd-Jones</span><span lang="EN-US" style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:gray">| Intelligent Automation Enablement Lead</span></p><p class="MsoNormal"><span lang="EN-US" style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:gray">Intelligent Automation CoE<br></span><span lang="EN-US" style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:#FF5800">Avanade</span><span lang="EN-US" style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif"> |<span style="color:gray">Direct: &#43;44 (0)20 81969867</span></span></p><p class="MsoNormal"><span style="font-size:11.0pt"><a href="http://www.avanade.com/"><span style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:#FF5800">www.avanade.com</span></a></span><span style="font-size:8.0pt; font-family:&quot;Segoe UI&quot;,sans-serif; color:#FF5800"></span></p>`
+
 
 var app = angular.module('taskTracker', []);
 
@@ -24,6 +26,7 @@ app.controller('emailShow', function($scope, $http, $sce) {
         $scope.DateTimeReceived="";$scope.daysAgo="";
         $scope.body="";$scope.subject="";$scope.Id="";
         $scope.ConversationId="";
+        $scope.textprocess=false;
         // unaware of a way to multi-assign as this is an object
     }
 
@@ -58,8 +61,10 @@ app.controller('emailShow', function($scope, $http, $sce) {
             let email=$scope.mailarray.shift();
             $scope.MessageId=email.Id;
             if ($scope.processedmails.indexOf($scope.MessageId) !=  -1) {
+                console.log("skipping previously actioned email");
                 $scope.showNextEmail();
             } else {
+                console.log(email);
                 console.log("shifted email with ID %s,",email.Id);  
                 $scope.body=$sce.trustAsHtml(email.Body);
                 $scope.from=email.From;
@@ -78,34 +83,67 @@ app.controller('emailShow', function($scope, $http, $sce) {
     }
 
     $scope.pushDown = function(myEvent) {
-        console.log("Pressed %s",myEvent.key);
-        if ($scope.processedmails.indexOf($scope.MessageId) ==  -1) {
-            $scope.processedmails.push($scope.MessageId);
-            $scope.sendAction(myEvent.key,
-                $scope.ConversationId,
-                $scope.MessageId);
-        } else {
-            console.log("Duplicate request for %s",$scope.MessageId);
+        if (!$scope.textprocess) {
+            console.log("Pressed %s",myEvent.key);
+            if ($scope.processedmails.indexOf($scope.MessageId) ==  -1) {
+                $scope.sendAction(myEvent.key,
+                    $scope.ConversationId,
+                    $scope.MessageId);
+            } else {
+                console.log("Duplicate request for %s",$scope.MessageId);
+            }
+        } else if (myEvent.keyCode === 13 && myEvent.ctrlKey) {
+            $scope.continueForm();
         }
     }
 
     $scope.sendAction = function(key,cid,mid) {
         let keyLowercase = key.toLowerCase();
-        if (!isNaN(key)) {
-            keyLowercase+='.';
-            console.log("appended . to %s",key);
+        if (keyLowercase=="z"||keyLowercase=="x"||keyLowercase=="b") {
+            $scope.textprocess=true;
+            $scope.continuekey=keyLowercase;
+        } else {
+            $scope.processedmails.push($scope.MessageId);
+            if (!isNaN(key)) {
+                keyLowercase+='.';
+                console.log("appended . to %s",key);
+            }
+            let dataObj = {
+                "action":keyLowercase,
+                "ConversationId":cid,
+                "MessageId":mid,
+                "body:":""
+            };
+            $scope.postForm(dataObj);
+            $scope.showNextEmail();
+            console.log("sending %s to be actioned",mid);
         }
-        let dataObj = {
-            "action":keyLowercase,
-            "ConversationId":cid,
-            "MessageId":mid
-        };
-        $scope.showNextEmail();
-        console.log("sending %s to be actioned",mid);
+    }
+
+    $scope.postForm = function(dataObj) {
         $http.post(url_actions,dataObj).then(function(response) {
-            console.log("email %s actioned",mid);
+            console.log("email %s actioned",dataObj.MessageId);
         });
-        
+    }
+
+    $scope.cancelForm=function() {
+        $scope.textprocess=false;
+        $scope.showNextEmail();
+    }
+
+    $scope.continueForm = function() {
+        $scope.textprocess=false;
+        let dataObj = {
+            "action":$scope.continuekey,
+            "ConversationId":$scope.ConversationId,
+            "MessageId":$scope.MessageId,
+            "body:":$scope.bodytext
+        };
+        $scope.postForm(dataObj);
+        $scope.subject="(processed) "+$scope.subject;
+        $scope.body=$sce.trustAsHtml("<div class='alert alert-success'><strong>Success!</strong> Email sent!</div>"+$scope.body);
+        console.log("sending %s to be actioned with bodytext %s",$scope.MessageId,$scope.bodytext);
+        $scope.bodytext="";
     }
 
     $scope.initVariables();
