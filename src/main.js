871565235db8cbf946a4250e5da2faf2dc5d6325
Chris Lloyd-Jones <chris.lloyd-jones@avanade.com>
2018-03-01 21:16:43 -0500

    All working

---
+++ src/main.js
@@ -0,0 +1,113 @@
+const url_actions="https://prod-04.uksouth.logic.azure.com:443/workflows/56254cab7b6c407f91aa7a94a9ef549d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d33x49ZsEIF7xa-u5Q9BKU2Adf6jlnCJRtwwDd2hvoM";
+// type, conversationid
+const url_emails="https://prod-29.uksouth.logic.azure.com:443/workflows/b136a37912e6424590967522192b3b35/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=askQTpkRwhuZH70FPRiqCXgCyhrbMfiI6GxwtLOFuI4";
+// nada
+
+var app = angular.module('taskTracker', []);
+
+app.controller('emailShow', function($scope, $http, $sce) {
+    // Init variables
+    // Reset display function
+    // Get emails
+    // Show next email
+    // Send action
+    // if emails run out, send another
+
+    $scope.initVariables = function() {
+        $scope.mailcount=0;
+        $scope.mailarray=new Array();
+        $scope.processedmails=new Array();
+    }
+
+    $scope.resetDisplay = function() {
+        $scope.from="";$scope.to="";$scope.cc="";
+        $scope.DateTimeReceived="";$scope.daysAgo="";
+        $scope.body="";$scope.subject="";$scope.Id="";
+        $scope.ConversationId="";
+        // unaware of a way to multi-assign as this is an object
+    }
+
+    $scope.refreshButton = function() {
+        $scope.resetDisplay();
+        $scope.mailarray=new Array();
+        $scope.showNextEmail();
+    }
+
+    $scope.getEmails = function(showEmail=true) {
+        $http.get(url_emails).then(function(response) {
+            console.log("downloaded next set of emails.");
+            $scope.mailarray=$scope.mailarray.concat(response.data);
+            if ($scope.mailarray.length > 0) {
+                if (showEmail) {
+                    $scope.showNextEmail();
+                }
+            } else {
+                console.log("out of emails.");
+                $scope.resetDisplay();
+                $scope.subject="out of emails";
+            }
+        });
+    }
+
+    $scope.showNextEmail = function() {
+        $scope.resetDisplay();
+        if ($scope.mailarray.length==0) {
+            console.log("array empty, refreshing array.");
+            $scope.getEmails();
+        } else {
+            let email=$scope.mailarray.shift();
+            $scope.MessageId=email.Id;
+            if ($scope.processedmails.indexOf($scope.MessageId) !=  -1) {
+                $scope.showNextEmail();
+            } else {
+                console.log("shifted email with ID %s,",email.Id);  
+                $scope.body=$sce.trustAsHtml(email.Body);
+                $scope.from=email.From;
+                $scope.cc=email.Cc;
+                $scope.to=email.To;
+                $scope.DateTimeReceived=email.DateTimeReceived;
+                $scope.ConversationId=email.ConversationId;
+                $scope.subject=email.Subject;
+                $scope.mailcount=$scope.mailarray.length + 1;
+            }
+            if ($scope.mailarray.length<2) {
+                console.log("email buffer is low, triggering download");
+                $scope.getEmails(false);
+            }
+        }
+    }
+
+    $scope.pushDown = function(myEvent) {
+        console.log("Pressed %s",myEvent.key);
+        if ($scope.processedmails.indexOf($scope.MessageId) ==  -1) {
+            $scope.processedmails.push($scope.MessageId);
+            $scope.sendAction(myEvent.key,
+                $scope.ConversationId,
+                $scope.MessageId);
+        } else {
+            console.log("Duplicate request for %s",$scope.MessageId);
+        }
+    }
+
+    $scope.sendAction = function(key,cid,mid) {
+        let keyLowercase = key.toLowerCase();
+        if (!isNaN(key)) {
+            keyLowercase+='.';
+            console.log("appended . to %s",key);
+        }
+        let dataObj = {
+            "action":keyLowercase,
+            "ConversationId":cid,
+            "MessageId":mid
+        };
+        $scope.showNextEmail();
+        console.log("sending %s to be actioned",mid);
+        $http.post(url_actions,dataObj).then(function(response) {
+            console.log("email %s actioned",mid);
+        });
+        
+    }
+
+    $scope.initVariables();
+    $scope.showNextEmail();
+});
